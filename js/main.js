51666cb39430146f5b3e8e0f96a5589ba36a6fe7
Tuyenlearning-1 <6051071139@st.utc2.edu.vn>
2021-08-18 18:58:53 +0700

    test kha ok

--- js/main.js
+++ js/main.js
@@ -12,13 +12,14 @@ AOS.init();
 
 // nav btn toggle 
 const toggleBtn = document.querySelector('.nav-toggle');
-const navFixed = document.querySelector('.header__nav--small');
-const navClose = document.querySelector('.header__nav--close');
+const navClose = document.querySelector('.nav--close');
+let navChange = document.querySelector('.nav--change');
+let navFixed = document.querySelector('.header__list');
 
 
 
 toggleBtn.addEventListener('click', function(){
-	navFixed.classList.toggle('active');
+		navFixed.classList.toggle('active');
 })
 navClose.addEventListener('click', function(){
 	navFixed.classList.remove('active');
